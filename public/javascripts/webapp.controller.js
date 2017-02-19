b4423656d6aab82fd93fd31d23f16f2d8aacd6db
Ubuntu <ubuntu@ip-172-31-26-168.ap-northeast-1.compute.internal>
2017-02-19 04:36:39 +0000

    first commit

---
+++ public/javascripts/webapp.controller.js
@@ -0,0 +1,50 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('webApp')
+        .controller('WebController', WebController);
+
+    function WebController($scope) {
+        var vm = this;
+        vm.images = [];
+        vm.queue = [];
+
+        var sound = new Howl({
+            src: ['assets/audio/horn.mp3']
+        });
+
+        vm.playhorn = function() {
+            sound.play();
+        };
+
+        vm.videos = [{
+            videoId: '1dLXp0_1b6I',
+            mute: false
+        }];
+
+        vm.submit = function(url) {
+            var id = url.split('=')[1];
+            var newvideo = {
+                videoId: id,
+                mute: false
+            };
+            var imageurl = "http://i.ytimg.com/vi/" + id + "/default.jpg";
+            var imageinfo = {
+                url: imageurl,
+                videoId: id
+            };
+
+            vm.images.push(imageinfo);
+            vm.queue.push(newvideo);
+            console.log(vm.images);
+        };
+
+        vm.playvideo = function(id) {
+            vm.videos[0] = {
+                videoId: id,
+                mute: false
+            };
+        };
+    };
+})();
